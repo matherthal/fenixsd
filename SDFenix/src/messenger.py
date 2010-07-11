d78eab5f28df0349282117df016cf2bbddfac502
giuliobottari <giuliobottari@18f6ab3f-0eb7-06be-4288-b93b3585c96d>
2010-07-11 22:21:22 +0000

    inserção da descrição da classe, tirei o constructor

--- SDFenix/src/messenger.py
+++ SDFenix/src/messenger.py
@@ -7,23 +7,27 @@ Created on 11/07/2010
 
 class Messenger(object):
     '''
-    classdocs
+    Classe estática
+    
+    É a entidade que faz interface com o programador da aplicação. 
+    Ele envia as mensagens com o send e receive, e por baixo dos panos, o Messenger 
+    conversa com o Coordinator para saber o que fazer.
+    Quando o programa da aplicação envia uma mensagem (que é uma String) através do 
+    send, o Messenger precisa adicionar o header de tolerância a falhas na mensagem, 
+    criando um objeto do tipo Message. Isso é feito através do método _insertHeader. 
+    A operação inversa (transformar um Message em String) é feita pelo _removeHeader. 
+    Além disso, para preencher essas informações, o Messenger precisa se comunicar 
+    com o Coordinator para poder obter a última sequencia recebida de um cliente. 
     '''
-
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
         
     def _insertHeader(self, data):
-        pass
+        raise NotImplementedError
     
     def _removeHeader(self, message):
-        pass
+        raise NotImplementedError
     
     def send(self, message):
-        pass
+        raise NotImplementedError
     
     def receive(self):
-        pass
+        raise NotImplementedError
