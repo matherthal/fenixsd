0c69dae2cb276229e8509aaf5ad89ab42024bcc5
giuliobottari <giuliobottari@18f6ab3f-0eb7-06be-4288-b93b3585c96d>
2010-07-11 22:24:25 +0000

    descrição da classe state

--- SDFenix/src/state.py
+++ SDFenix/src/state.py
@@ -7,13 +7,16 @@ Created on 11/07/2010
 
 class State(object):
     '''
-    classdocs
+    Classe que mantém o estado entre a máquina em questão e uma outra. 
+    Nesse sentido, o State mantém o estado de uma comunicação cliente-servidor.
+    Sendo assim, é necessário guardar o id do cliente o qual está ocorrendo a 
+    comunicação e o último número de sequência recebido (last_sequence).
     '''
 
 
-    def __init__(self):
+    def __init__(self, id):
         '''
         Constructor
         '''
         self.lastSequence = 0
-        self.id = ""
+        self.id = id
